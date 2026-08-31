d559c0266b493fd5640a066683a563e6ee11035a
Pedro <souzapedro687@gmail.com>
2026-08-31 20:25:22 -0300

    repetições JS

---
+++ Aula11 - Repetições/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+do{
+    console.log(`Passo número ${c}`)
+    c++
+}while(c < 1)
+
+
+/*
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+console.log ('Tudo Bem?')
+*/
